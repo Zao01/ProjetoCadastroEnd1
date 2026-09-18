aff027d03e1e8f7db0a94748bd0a01764ea2ffe9
Zao01 <adm.marcosfonseca@gmail.com>
2026-09-17 22:56:07 -0300

    chore: atualiza .gitignore e remove arquivos sensíveis do rastreamento

--- OneDrive/Desktop/PROJETO/desafio-analista-sustentacao/backend/src/main/java/com/desafio/sustentacao/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.desafio.sustentacao.entity;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_USUARIO
-}
--- OneDrive/Desktop/PROJETO/desafio-analista-sustentacao/backend/src/main/java/com/desafio/sustentacao/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.desafio.sustentacao.repository;
-
-import com.desafio.sustentacao.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-    List<Endereco> findByUsuarioId(Long usuarioId);
-}
